a3e6eeeb31e4418d099b63eda015868559fc8209
ssc19940105 <ssc19940105@163.com>
2026-08-23 10:26:10 +0800

    fix(controls): 添加 SfDataGrid 和 SfDataPager 的卸载清理行为

---
+++ BG3PakViewer.Controls/Behaviors/SfDataGridBehavior.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using Microsoft.Xaml.Behaviors;
+using Syncfusion.UI.Xaml.Grid;
+
+namespace BG3PakViewer.Controls.Behaviors;
+
+public class SfDataGridBehavior : Behavior<SfDataGrid>
+{
+    protected override void OnAttached()
+    {
+        AssociatedObject.Unloaded += AssociatedObject_Unloaded;
+    }
+
+    protected override void OnDetaching()
+    {
+        AssociatedObject.Unloaded -= AssociatedObject_Unloaded;
+    }
+
+    private void AssociatedObject_Unloaded(object sender, RoutedEventArgs e)
+    {
+        AssociatedObject.SearchHelper.Dispose();
+        AssociatedObject.Dispose();
+    }
+}
---
+++ BG3PakViewer.Controls/Behaviors/SfDataPagerBehavior.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using Microsoft.Xaml.Behaviors;
+using Syncfusion.UI.Xaml.Controls.DataPager;
+
+namespace BG3PakViewer.Controls.Behaviors;
+
+public class SfDataPagerBehavior : Behavior<SfDataPager>
+{
+    protected override void OnAttached()
+    {
+        AssociatedObject.Unloaded += AssociatedObject_Unloaded;
+    }
+
+    protected override void OnDetaching()
+    {
+        AssociatedObject.Unloaded -= AssociatedObject_Unloaded;
+    }
+
+    private void AssociatedObject_Unloaded(object sender, RoutedEventArgs e)
+    {
+        AssociatedObject.Dispose();
+    }
+}
